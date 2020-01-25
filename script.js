9a5e0449a90b3c86b78779e95965f5eb2bd1715b
JeffNJellyBean <jeffnjellybean@gmail.com>
2020-01-25 10:14:21 -0500

    add assets and basic thingy but cors is not good boi

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function muffin() {
-   
+const WIDTH = 720;
+const HEIGHT = 480;
+const app = new PIXI.Application({
+  width: WIDTH,
+  height: HEIGHT
+});
+document.body.appendChild(app.view);
 
-}setTimeout 
+const stage = app.stage;
+
+const barn = new PIXI.Sprite.from('assets/barn.jpeg');
+barn.anchor.set(0.5, 0.5);
+barn.position.set(WIDTH / 2, HEIGHT / 2);
+barn.width = WIDTH;
+barn.height = HEIGHT;
+
+const cow = new PIXI.Sprite.from('assets/cow.png');
+
+stage.addChild(barn);
+stage.addChild(cow);
